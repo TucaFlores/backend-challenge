9e0c708eef715c1bea72dff323c1f86c44dd2c0a
Tuca Flores <sebastian.flores@sitrack.com>
2025-10-18 14:10:03 +0000

    Se agrega al servicio la persistencia de la actividad de mail

--- src/main/java/com/myhotel/template/services/SurveyNotificationService.java
+++ src/main/java/com/myhotel/template/services/SurveyNotificationService.java
@@ -1,16 +1,23 @@
 package com.myhotel.template.services;
+import com.myhotel.template.entities.ActivityMails;
 import com.myhotel.template.models.MessageResult;
+import com.myhotel.template.repositories.ActivityMailsRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class SurveyNotificationService {
 
     private final EmailSenderService emailSenderService;
     private final EmailTemplateService emailTemplateService;
+    private final ActivityMailsRepository activityMailsRepository;
 
-    public SurveyNotificationService(EmailSenderService emailSenderService, EmailTemplateService emailTemplateService) {
+    public SurveyNotificationService(EmailSenderService emailSenderService, EmailTemplateService emailTemplateService,  ActivityMailsRepository activityMailsRepository) {
         this.emailSenderService = emailSenderService;
         this.emailTemplateService = emailTemplateService;
+        this.activityMailsRepository = activityMailsRepository;
     }
 
     public MessageResult notifyGuest(Long surveyResponseId) {
@@ -18,6 +25,26 @@ public class SurveyNotificationService {
         String to = emailTemplateService.getRecipe(surveyResponseId);
         String subject = "Thank you for your feedback!";
         String body = emailTemplateService.buildMailMessage(surveyResponseId);
-        return emailSenderService.sendEmail(from, to, subject, body);
+
+        MessageResult messageResult = emailSenderService.sendEmail(from, to, subject, body);
+
+        ActivityMails activityMails = new ActivityMails();
+        activityMails.setSurveyId(surveyResponseId);
+        activityMails.setFrom(from);
+        activityMails.setTo(to);
+        activityMails.setSubject(subject);
+        activityMails.setMessage(body);
+        activityMails.setSentAt(LocalDateTime.now());
+        activityMailsRepository.save(activityMails);
+
+        return messageResult;
+    }
+
+    public List<ActivityMails> getEmailHistory(List<Long> surveyIds) {
+        if (surveyIds != null && !surveyIds.isEmpty()) {
+            return activityMailsRepository.findAllBySurveyIdIn(surveyIds);
+        } else {
+            return activityMailsRepository.findAll();
+        }
     }
 }

b8556c29956d69b7e4cbfb47d465e426ec638f56
Tuca Flores <sebastian.flores@sitrack.com>
2025-10-18 14:20:52 +0000

    Refactorizacion buscando peso y calculando con el peso

--- src/main/java/com/myhotel/template/projections/SurveyScoreGroupProjection.java
+++ src/main/java/com/myhotel/template/projections/SurveyScoreGroupProjection.java
@@ -9,11 +9,11 @@ public interface SurveyScoreGroupProjection {
     String getHotelName();
     Long getHotelId();
     Long getScore();
-    Long getScoreCount();
+    Long getWeight();
 
     default double weightedScore() {
         Long score = Optional.ofNullable(getScore()).orElse(DEFAULT_VAL);
-        Long count = Optional.ofNullable(getScoreCount()).orElse(DEFAULT_VAL);
-        return score * count;
+        Long weight = Optional.ofNullable(getWeight()).orElse(DEFAULT_VAL);
+        return score * weight;
     }
 }
